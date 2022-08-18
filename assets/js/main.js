b7deec38fc21a4444ae548dc1b7e02a5a1394ff6
Cakctus <cakctusinc@gmail.com>
2022-08-18 12:16:58 +0300

    add editor config

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
   $(".header-navbar__hamburger").on("click", function () {
-    $(this).toggleClass("active")
-    $(".header-navbar__list").toggleClass("active")
-  })
-})
+    $(this).toggleClass("active");
+    $(".header-navbar__list").toggleClass("active");
+  });
+});
 
 $(document).ready(function () {
   $(".header-navbar__burger").click(function () {
-    $(this).toggleClass("header-navbar__burger--open")
-    $(".header-navbar__list").stop(true, true).slideToggle(500)
-  })
-})
+    $(this).toggleClass("header-navbar__burger--open");
+    $(".header-navbar__list").stop(true, true).slideToggle(500);
+  });
+});
